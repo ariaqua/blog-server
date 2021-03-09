082249907f9541ebd3d1a82d6031679359e2c8e4
godline <lumywork@163.com>
2021-03-09 17:58:25 +0800

    sync code

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,21 +1,66 @@
-const commentsWrapper = document.querySelector('.comments');
+const commentsWrapper = document.querySelector('.comments-wrapper');
 
 const articleId = location.pathname.split('/').pop();
 
-let comments,
-  str = '';
-
 fetch(`api/comment/${articleId}`).then(async (data) => {
+  let comments,
+    str = '',
+    deepStr = '';
+
   comments = await data.json();
-  console.log(comments);
-  comments.data.forEach((comment) => {
-    str += `
-      <div style="background: whitesmoke">
-        <p>${comment.avatar}-${comment.alia}-${comment.date}</p>
-        <p>${comment.comment}</p>
-      </div>
-      `;
-  });
-  console.log(str);
+
+  if (!comments.data.length) {
+    str = '<p>暂无评论</p>';
+  } else {
+    comments.data.forEach((comment) => {
+      let subStr = '';
+      if (comment.children.length) {
+        comment.children.forEach((subComment) => {
+          if (subComment.deep_reply_id) {
+            deepStr = `
+              <span style="color: tomato"><span class="at">@</span>${subComment.deep_reply_name}</span>
+              `;
+          }
+
+          subStr += `
+            <div class="sub-comment-wrapper">
+              <div class="head">
+                <img class="avatar" src="${subComment.avatar}" />
+                <div class="alia_time-wrapper">
+                  <div>
+                    <span class="alia">${subComment.alia}</span>
+                    <span>${deepStr}</span>
+                  </div>
+                  <div class="time">
+                    ${dayjs(subComment.date).format(' YYYY-MM-DD HH: mm')}
+                  </div>
+                </div>
+              </div>
+              <p class="content">${subComment.comment}</p>
+            </div>
+            `;
+        });
+      }
+
+      str += `
+          <div class="comment-wrapper">
+            <div class="head">
+              <img class="avatar" src="${comment.avatar}" />
+              <div class="alia_time-wrapper">
+                <div class="alia">${comment.alia}</div>
+                <div  class="time">
+                  ${dayjs(comment.date).format(' YYYY-MM-DD HH: mm')}
+                </div>
+              </div>
+              
+            </div>
+            
+            <p class="content">${comment.comment}</p>
+            ${subStr}
+          </div>
+          `;
+    });
+  }
+
   commentsWrapper.innerHTML = str;
 });
